39cd73b48cb32d5a650d19d4d22a93fbf6746ee1
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-04-06 16:30:09 +0530

    added tls

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient } = require('mongodb')
+const { MongoClient, Db } = require('mongodb')
 
 const app = express()
 const port = 3000
@@ -10,56 +10,63 @@ app.use(express.json())
 
 const uri = 'mongodb://localhost:27017'
 
-MongoClient.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(client => {
+const withdb = async (cb) => {
+  try {
+    const client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    const db = client.db('test')
+    return cb({
+      moods: db.collection('moods'),
+      thoughts: db.collection('thoughts'),
+      journal: db.collection('journal')
+    })
+  } catch (error) {
+    console.log(error)
+    return error
+  }
+}
 
-  const db = client.db('test')
-  const moods = db.collection('moods')
-  const thoughts = db.collection('thoughts')
-  const journals = db.collection('journals')
+app.get('/moods', async (req, res) => {
+  console.log('GET /moods', req.body)
+  const moods = await withdb(({ moods }) => moods.find().toArray())
+  res.json(moods)
+})
 
-  app.get('/moods', async (req, res) => {
-    console.log('GET /moods', req.body)
-    const moods = await moods.find().toArray()
-    res.json(moods)
-  })
+app.post('/moods', async (req, res) => {
+  const { mood } = req.body
+  console.log('POST /moods', mood)
+  await withdb(({ moods }) => moods.insertOne({ mood }))
+  res.json({ message: 'Mood saved!' })
+})
 
-  app.post('/moods', async (req, res) => {
-    const { mood } = req.body
-    console.log('POST /moods', mood)
-    await moods.insertOne({ mood })
-    res.json({ message: 'Mood saved!' })
-  })
+app.get('/thoughts', async (req, res) => {
+  console.log('GET /thoughts', req.body)
+  const thoughts = await withdb(({ thoughts }) => thoughts.find().toArray())
+  res.json(thoughts)
+})
 
-  app.get('/thoughts', async (req, res) => {
-    console.log('GET /thoughts', req.body)
-    const thoughts = await thoughts.find().toArray()
-    res.json(thoughts)
-  })
+app.post('/thoughts', async (req, res) => {
+  const { thought } = req.body
+  console.log('POST /thoughts', thought)
+  await withdb(({ thoughts }) => thoughts.insertOne({ thought }))
+  res.json({ message: 'Thought saved!' })
+})
 
-  app.post('/thoughts', async (req, res) => {
-    const { thought } = req.body
-    console.log('POST /thoughts', thought)
-    await thoughts.insertOne({ thought })
-    res.json({ message: 'Thought saved!' })
-  })
+app.get('journal', async (req, res) => {
+  console.log('GET /journal', req.body)
+  const journal = await withdb(({ journal }) => journal.find().toArray())
+  res.json(journal)
+})
 
-  app.get('journal', async (req, res) => {
-    console.log('GET /journal', req.body)
-    const journal = await journals.find().toArray()
-    res.json(journal)
-  })
+app.post('/journal', async (req, res) => {
+  const { journal } = req.body
+  console.log('POST /journal', journal)
+  await withdb(({ journal }) => journal.insertOne({ journal }))
+  res.json({ message: 'Journal saved!' })
+})
 
-  app.post('/journal', async (req, res) => {
-    const { journal } = req.body
-    console.log('POST /journal', journal)
-    await journals.insertOne({ journal })
-    res.json({ message: 'Journal saved!' })
-  })
-
-  app.listen(port, () => {
-    console.log(`server listening at http://localhost:${port}`)
-  })
+app.listen(port, () => {
+  console.log(`server listening at http://localhost:${port}`)
 })
