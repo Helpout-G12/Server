af1e711b08261c18bdaad63610ec1ef35d32dade
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-04-26 09:34:47 +0530

    f

--- openAI/clean_outputs.py
+++ openAI/clean_outputs.py
@@ -2,20 +2,17 @@ import os
 import re
 
 output_files = sorted(os.listdir('outputs'), key=lambda x: int(x.split('.')[0]))
-output = []
+output = ""
 
 for file in output_files:
-    with open('outputs/' + file) as f:
-        try:
-            line = f.readlines()[0].strip()
-        except:
-            continue
-        try:
-            question, answer = re.match(r'.*?{"user": "(.*?)", "therapist": "(.*?)"?[})]?.*', line).groups()
-        except:
-            print(line)
-            continue
-        output.append((question, answer))
+  with open('outputs/' + file) as f:
+    line = f.readline().strip()
+    if not line: continue
+    line = line.split('"user":')[1].strip()
+    try:
+        question, answer, *_ = line.split('"therapist":')
+    except:
+        print(file, line)
+    output+="question: " + question + '\n' + "answer: "+ answer + '\n\n'
 
-print(output, file=open('clean-output.txt', 'w'))
-
+print(output, file=open('clean-output.txt', 'w+'))
